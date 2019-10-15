767121871ea2c3fc511ec585da74eee221a95d22
Harut Muqoyan <hmuqoyan@gmail.com>
2019-10-15 12:27:28 +0400

    added App description

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ import './i18n';
 function App() {
   return (
     <div className="App">
+      <div className='appDescription'>
+        <p>App Description</p>
+        <p>A simple signin signup react app which has an ability to detect</p>
+        <ul>
+          <li>browser default language</li>
+          <li>switch the whole app language</li>
+        </ul>
+          <p>To implement mock request, it's used Redux /Redux Saga</p>
+      </div>
       <Provider store={store} >
         <LangNav />
         <Suspense fallback={<h1>Loading</h1>}>
